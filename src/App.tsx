388f67b593f0e6ead29b2f635a5aee128a660755
Alexandra <alexbushueva27@gmail.com>
2021-02-04 23:49:03 +0300

    12 - components

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="app-wrapper">
-      <header className={"header"}>
-        <img src={'https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg'} />
-      </header>
-      <nav className={"nav"}>
-          <div>
-              <a>Profile</a>
-          </div>
-          <div>
-              <a>Messages</a>
-          </div>
-          <div>
-              <a>News</a>
-          </div>
-          <div>
-              <a>Music</a>
-          </div>
-          <div>
-              <a>Settings</a>
-          </div>
-
-      </nav>
-      <div className={"content"}>
-          <img src={"https://img.freepik.com/free-vector/purple-3d-modern-background-design_53876-87399.jpg?size=626&ext=jpg&ga=GA1.2.1464808425.1601683200"} />
-        <div>
-            Main content
-        </div>
-          <div>
-              ava+description
-          </div>
-          <div>
-              My posts
-              <div>
-                  New post
-              </div>
-          </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className={"content"}>
+        <img src={"https://img.freepik.com/free-vector/purple-3d-modern-background-design_53876-87399.jpg?size=626&ext=jpg&ga=GA1.2.1464808425.1601683200"} />
+        <div>
+            Main content
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
