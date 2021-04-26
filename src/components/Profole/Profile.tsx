13241befac541174cc1d72b71841826ec89ada68
Alexandra <alexbushueva27@gmail.com>
2021-04-26 23:01:19 +0300

    14 - css-modules

--- src/components/Profole/Profile.tsx
+++ src/components/Profole/Profile.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import s from "./Profile.module.css"
 
 const Profile = () => {
-    return <div className={"content"}>
+    return <div className={s.content}>
         <img src={"https://img.freepik.com/free-vector/purple-3d-modern-background-design_53876-87399.jpg?size=626&ext=jpg&ga=GA1.2.1464808425.1601683200"} />
         <div>
             Main content
@@ -11,7 +12,7 @@ const Profile = () => {
         </div>
         <div>
             My posts
-            <div>
+            <div className={s.item}>
                 New post
             </div>
         </div>
