885ae31ab7a2c77e3fe9d858e6bc4614bd6f09de
Alexandra <alexbushueva27@gmail.com>
2021-04-28 00:33:47 +0300

    21 - mark up Dialogs component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,34 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Dima
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi!
+                </div>
+                <div className={s.message}>
+                    I'm awesome!
+                </div>
+                <div className={s.message}>
+                    How are you?
+                </div>
+
+            </div>
         </div>
     )
 }
