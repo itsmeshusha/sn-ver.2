80939d01bba78af35c50bf2155adfa1fc3a0d260
Alexandra <alexbushueva27@gmail.com>
2021-06-20 23:08:08 +0300

    Add Users page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
+import {Users} from "./components/Users/Users";
 
 function App() {
     return (
@@ -21,6 +22,7 @@ function App() {
                     <Route path={'/news'} render={() => <News />}/>
                     <Route path={'/music'} render={() => <Music />}/>
                     <Route path={'/settings'} render={() => <Settings />}/>
+                    <Route path={'/users'} render={() => <Users />}/>
 
                 </div>
             </div>
