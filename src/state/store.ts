5e7c03071836dd07c48689874d757f2aff26aa41
itsmeshusha <alexbushueva27@gmail.com>
2021-07-26 23:12:08 +0300

    Add redux-form

--- src/state/store.ts
+++ src/state/store.ts
@@ -4,6 +4,7 @@ import {profileReducer} from "./profileReducer";
 import {usersReducer} from "./usersReducer";
 import thunkMiddleware from 'redux-thunk';
 import {authReducer} from "./authReducer";
+import { reducer as formReducer } from 'redux-form';
 
 
 export type AppRootStateType = ReturnType<typeof reducers>
@@ -12,7 +13,11 @@ const reducers = combineReducers({
     dialogsPage: dialogsReducer,
     profilePage: profileReducer,
     users: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 export const store = createStore(reducers, applyMiddleware(thunkMiddleware))
+
+//@ts-ignore
+window.store = store
