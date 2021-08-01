a0e405616c7cecf1c39e966a21eb163498e3d62c
itsmeshusha <alexbushueva27@gmail.com>
2021-08-02 00:15:05 +0300

    Refactoring FormControl

--- src/assets/FormControls/FormControl.tsx
+++ src/assets/FormControls/FormControl.tsx
@@ -1,25 +1,31 @@
 import React from 'react'
 import s from './FormControl.module.css'
 
-export const Textarea = ({input, meta, ...props}: any) => {
+const FormControl = ({input, meta, ...props}: any) => {
     const error = meta.touched && meta.error
     return (
         <div className={`${s.formControl} ${error ? s.error : ''}`}>
             <div>
-                <textarea {...input} {...props} />
+                {props.children}
             </div>
             {error && <span>{meta.error}</span>}
         </div>
     )
 }
-export const Input = ({input, meta, ...props}: any) => {
-    const error = meta.touched && meta.error
+
+export const Textarea = (props: any) => {
+    const {input, meta, ...restProps} = props
     return (
-        <div className={`${s.formControl} ${error ? s.error : ''}`}>
-            <div>
-                <input {...input} {...props} />
-            </div>
-            {error && <span>{meta.error}</span>}
-        </div>
+        <FormControl {...props}>
+            <textarea {...input} {...restProps} />
+        </FormControl>
+    )
+}
+export const Input = (props: any) => {
+    const {input, meta, ...restProps} = props
+    return (
+        <FormControl {...props}>
+            <input {...input} {...restProps} />
+        </FormControl>
     )
 }
